5e50f4380b658e6fd3e5d6d17a19b7517c5bbebd
LeeJuCheon <ehrn2050@naver.com>
2022-06-21 15:58:36 +0900

    1

--- catkin_bash.sh
+++ catkin_bash.sh
@@ -1,4 +1,6 @@
 # Download
+cd ~/
+cd ..
 cd /tmp
 CERES_VERSION="ceres-solver-2.1.0"
 CERES_ARCHIVE="$CERES_VERSION.tar.gz"
@@ -40,28 +42,3 @@ make -j2
 make install
 apt-get install -y apt-utils
 
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-
-touch /etc/ld.so.conf.d/g2o.conf
-echo "/root/catkin_ws/devel/lib" >> /etc/ld.so.conf.d/g2o.conf
-
-echo "catkin_init_workspace"
-mkdir -p ~/catkin_ws/src 
-cd ~/catkin_ws/src 
-catkin_init_workspace
-
-echo "g2o_catkin"
-cd ~/catkin_ws/src
-git clone https://github.com/yorsh87/g2o_catkin.git
-cd ..
-catkin build g2o_catkin
-
-echo "buildDeps.py"
-cd ~/catkin_ws/src
-cd proSLAM_docker
-chmod +x ./pull_srrg_packages.bash
-./pull_srrg_packages.bash
-cd .. 
-catkin build --make-args tests -- srrg_proslam
-catkin build srrg_proslam
