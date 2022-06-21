5e0afd8f058873b616d73efdf69c168fd3759bef
LeeJuCheon <ehrn2050@naver.com>
2022-06-21 11:16:52 +0900

    a

--- catkin_bash.sh
+++ catkin_bash.sh
@@ -15,23 +15,23 @@ source ~/.bashrc && \
 touch /etc/ld.so.conf.d/g2o.conf
 echo "/root/catkin_ws/devel/lib" >> /etc/ld.so.conf.d/g2o.conf
 
-echo "catkin_init_workspace"
-mkdir -p ~/catkin_ws/src 
-cd ~/catkin_ws/src 
-catkin_init_workspace
+# echo "catkin_init_workspace"
+# mkdir -p ~/catkin_ws/src 
+# cd ~/catkin_ws/src 
+# catkin_init_workspace
 
-echo "g2o_catkin"
-cd ~/catkin_ws/src
-git clone https://github.com/yorsh87/g2o_catkin.git
-cd ..
-catkin build g2o_catkin
+# echo "g2o_catkin"
+# cd ~/catkin_ws/src
+# git clone https://github.com/yorsh87/g2o_catkin.git
+# cd ..
+# catkin build g2o_catkin
 
-echo "buildDeps.py"
-cd ~/catkin_ws/src
-cd proSLAM_docker
-python3 ./buildDeps.py --d --system
-chmod +x ./pull_srrg_packages.bash
-./pull_srrg_packages.bash
-cd .. 
-catkin build --make-args tests -- srrg_proslam
-# catkin build srrg_proslam
+# echo "buildDeps.py"
+# cd ~/catkin_ws/src
+# cd proSLAM_docker
+# python3 ./buildDeps.py --d --system
+# chmod +x ./pull_srrg_packages.bash
+# ./pull_srrg_packages.bash
+# cd .. 
+# catkin build --make-args tests -- srrg_proslam
+# # catkin build srrg_proslam
